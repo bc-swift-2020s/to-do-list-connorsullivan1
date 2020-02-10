7aa80bc55c555163b12dc7d87a4e98afa876d3e5
Connor Sullivan <sulliamr@bc.edu>
2020-02-10 00:28:31 -0500

    5.6

---
+++ ToDoList/ToDoItem.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoItem.swift
+//  ToDoList
+//
+//  Created by Connor on 2/10/20.
+//  Copyright © 2020 Connor Sullivan. All rights reserved.
+//
+
+import Foundation
+
+struct ToDoItem {
+    var name: String
+    var date: Date
+    var notes: String
+}
